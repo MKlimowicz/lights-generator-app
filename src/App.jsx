788fb37266414e42e28f455c4212627475b739e7
marcin.klimowicz <marcin.klimowicz@iterative.pl>
2023-09-11 11:53:02 +0200

    feat: generete-lights

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,61 @@ import "./App.css";
 import { Counter } from "./components/Counter.jsx";
 import { Text } from "./components/Text.jsx";
 import { SingleLight } from "./components/SingleLight.jsx";
+import { Grid } from "./components/Grid.jsx";
+import { useState } from "react";
 
 function App() {
+  const [rows, setRows] = useState(0);
+  const [columns, setColumns] = useState(0);
+
+  const handleIncrementRow = () => {
+    setRows(rows + 1);
+  };
+
+  const handleDecrementRow = () => {
+    if (rows > 0) {
+      setRows(rows - 1);
+    }
+  };
+
+  const handleIncrementColumn = () => {
+    if (columns < 9) {
+      setColumns(columns + 1);
+    }
+  };
+
+  const handleDecrementColumn = () => {
+    if (columns > 0) {
+      setColumns(columns - 1);
+    }
+  };
+
+  const totalLights = rows * columns;
+
+  const lights = Array.from({ length: totalLights });
+
   return (
     <div>
       <nav className={"nav"}>
-        <Text> 0 bulbs </Text>
-        <Counter count={4} name={"rows"} />
-        <Counter count={3} name={"columns"} />
+        <Text> {totalLights} bulbs </Text>
+        <Counter
+          count={rows}
+          name={"rows"}
+          handleIncrement={handleIncrementRow}
+          handleDecrement={handleDecrementRow}
+        />
+        <Counter
+          count={columns}
+          name={"columns"}
+          handleIncrement={handleIncrementColumn}
+          handleDecrement={handleDecrementColumn}
+        />
       </nav>
-      <div>
-        <SingleLight />
-      </div>
+      <Grid columns={columns}>
+        {lights.map((value, index) => {
+          return <SingleLight key={{ index }} />;
+        })}
+      </Grid>
     </div>
   );
 }
